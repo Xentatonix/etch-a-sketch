3fba3e1bf5761377e660cc1fa6e9e0d9057bffc9
Xentatonix <14041598+Xentatonix@users.noreply.github.com>
2023-08-06 10:41:59 +0100

    added hold-down drawing

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let gridInput = document.querySelector("#gridInput");
 const gridSize = 600;
 grid.style.setProperty('--gridSize', `${gridSize}px`);
 
-let pixelNodeList;
-
 let changeColour = e => {
     e.target.style.backgroundColor = "black";
 }
@@ -19,12 +17,37 @@ let createPixels = e => {
         pixel.style.setProperty('--pixelSize', `${gridSize/e.target.value}px`);
         pixel.classList.add("pixel");
         
-        pixel.addEventListener("mousedown", changeColour)
-
         grid.appendChild(pixel);
     };
 
 }
 
-gridInput.addEventListener("input", createPixels);
+gridInput.addEventListener("change", createPixels);
 
+
+let activateColourEvent = e => {
+    e.srcElement.style.backgroundColor = "black";
+
+    let PixelNodeList = document.querySelectorAll(".pixel");
+
+    PixelNodeList.forEach(
+        pixel => {
+            pixel.addEventListener("mouseover", changeColour)
+        }
+    );
+}
+
+let deactivateColourEvent = e => {
+
+    let PixelNodeList = document.querySelectorAll(".pixel");
+
+    PixelNodeList.forEach(
+        pixel => {
+            pixel.removeEventListener("mouseover",changeColour);
+        }
+    )
+}
+
+
+grid.addEventListener("mousedown", activateColourEvent)
+grid.addEventListener("mouseup", deactivateColourEvent)
